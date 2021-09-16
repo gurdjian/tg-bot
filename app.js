a1b2072388a33b8be29335b1498d7f0601f4d02c
Алина <alina.12.98@mail.ru>
2021-09-16 11:14:05 +0300

    add start

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ const { PORT, TOKEN } = require('./config');
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const { Telegraf } = require('telegraf');
-const { getMainMenu } = require('./keyboard');
+const { getCards } = require('./keyboard');
+// const { User } = require('./db/models');
+// const { Adapter } = require('./db/models');
+const { Controller } = require('./controller');
 
 const app = express();
 const bot = new Telegraf(TOKEN);
 
 bot.start(ctx => {
-  ctx.reply('Welcome, bro', getMainMenu());
+  Controller.getAdapters(ctx.from);
+  ctx.reply('Выбери видеокарту', getCards());
+  // console.log(ctx.from.first_name)
+  // ctx.reply('Выбери разработчика видеокарты', getCardDeveloper());
 });
+
+// bot.action(['yes', 'no'], ctx => {
+//   if (ctx.callbackQuery.data === 'yes') {
+//       addTask('сюда будем передавать текст задачи')
+//       ctx.editMessageText('Ваша задача успешно добавлена')
+//   } else {
+//       ctx.deleteMessage()
+//   }
+// })
 
 bot.hears('хочу есть', ctx => {
   ctx.reply('Так передохни и покушай');
--- keyboard.js
+++ keyboard.js
@@ -1,9 +1,20 @@
 const { Markup } = require('telegraf');
+const { Controller } = require('./controller');
 
-function getMainMenu() {
-  return Markup.keyboard([
-    ['NVIDIA', 'AMD'],
+// массив объектов [{id: 1, title: 'fjdnv'}, {id: 2, title: 'fnvdkjfs'}]
+function getValue(arr) {
+  let newArr = [];
+  for (let obj of arr) {
+    newArr.push(obj.title);
+  }
+  return newArr;
+}
+
+function getCards() {
+  return Markup.inlineKeyboard([
+    Markup.button.callback(`${Controller.getAdapters.getValue(Controller.getAdapters())}`, `${Controller.getAdapters.getValue(Controller.getAdapters())}`),
+    // Markup.button.callback('AMD', 'AMD'),
   ]).resize();
 }
 
-module.exports = { getMainMenu };
+module.exports = { getCards };
