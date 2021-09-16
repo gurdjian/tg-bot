adeb3624393d37ce88a7b6caef4e24c013bfc980
Алина <alina.12.98@mail.ru>
2021-09-16 09:55:51 +0300

    First test virsion

---
+++ app.js
@@ -0,0 +1,27 @@
+const { PORT, TOKEN } = require('./config');
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf } = require('telegraf');
+const { getMainMenu } = require('./keyboard');
+
+const app = express();
+const bot = new Telegraf(TOKEN);
+
+bot.start(ctx => {
+  ctx.reply('Welcome, bro', getMainMenu());
+});
+
+bot.hears('хочу есть', ctx => {
+  ctx.reply('Так передохни и покушай');
+});
+
+bot.command('time', ctx => {
+  ctx.reply(String(new Date()));
+});
+
+bot.on('text', ctx => {
+  ctx.reply('just text');
+});
+
+bot.launch()
+app.listen(PORT, () => console.log(`My server is running on port ${PORT}`));
